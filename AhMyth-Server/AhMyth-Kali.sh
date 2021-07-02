855be3aa4efe575da3d695c863295fb468cd13c5
Morsmalleo <64344168+Morsmalleo@users.noreply.github.com>
2021-07-02 01:37:45 -0400

    Add files via upload
    relaced "AhMyth.sh" with new script "AhMyth-Kali.sh" which will run on Kali Linux without any problems or any needs to edit the file.
    Replaced "AhMyth-parrot.sh" with new script "AhMyth-Parrot.sh" which will run on Parrot OS without any problems or any needs to edit the file.
    Replaced "AhMyth-2.sh" with new script "AhMyth-Debian.sh" which will run on Debian and most Debian derived Linux distros without any errors or any needs to edit the file.

---
+++ AhMyth-Server/AhMyth-Kali.sh
@@ -0,0 +1,60 @@
+#/bin/sh
+
+# Ensure we are being ran as root
+if [ $(id -u) -ne 0 ]; then
+	echo "This script must be ran as root"
+	exit 1
+fi
+
+clear
+echo " |-------------------------| "
+echo " | Installing Prerequisits | "
+echo " |-------------------------| "
+sleep 2
+
+apt-get install xterm -y # Needed for the automatic software repo swap and backup
+apt-get install npm nodejs -y
+clear
+sleep 1
+
+echo " |----------------------| "
+echo " | Installing openJDK 8 | "
+echo " |----------------------| "
+sleep 2
+
+cp /etc/apt/sources.list /etc/apt/sources.list.backup # backup
+# Second backup created in case user stops the script after this point , then on next startup this script will
+# copy the already changed sources file before as backup, in case user lost his original sources lists
+file="/etc/apt/sources.list.stretch"
+ [ ! -f "$file" ]
+cp /etc/apt/sources.list /etc/apt/sources.list.stretch
+rm -f /etc/apt/sources.list
+touch /etc/apt/sources.list
+echo "deb http://deb.debian.org/debian stretch main contrib non-free" > /etc/apt/sources.list
+xterm -T " Updating Repositories Debian Stretch " -geometry 100x30 -e "apt-get clean && apt-get clean cache && apt-get update -y"
+sleep 2
+
+apt-get install -y openjdk-8-jdk* openjdk-8-jre*
+
+xterm -T " Reactivating Your Original Repositories " -geometry 100x30 -e "rm -f /etc/apt/sources.list && cp /etc/apt/sources.list.backup /etc/apt/sources.list"
+#now we can remove the emergency backup securely
+xterm -T " Removing emergency backup securly " -geometry 100x30 -e "rm -f /etc/apt/sources.list.stretch && rm -f /etc/apt/sources.list.backup"
+apt-get clean
+xterm -T " Updating Your Original Repositories " -geometry 100x30 -e "apt-get update"
+clear
+sleep 2
+
+echo " |-------------------| "
+echo " | Installing AhMyth | "
+echo " |-------------------| " 
+su -c "npm install && npm audit fix" kali
+clear
+/usr/bin/update-alternatives --config java
+clear
+echo " |---------------------------| "
+echo " | AhMyth has Been installed | "
+echo " |---------------------------| "
+sleep 2
+clear
+exit 0 
+fi
